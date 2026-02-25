835e17a54642e462501914c64badfed6b4be7adc
Mariana Fernandes <marianaflopes155@gmail.com>
2026-02-24 22:12:12 -0300

    Aula 1 - Variavies em JavaScript

---
+++ Aula1/scrip.js
@@ -0,0 +1,24 @@
+//Exemplos de variável let
+
+console.log('Testando JavaScript');
+
+let nome = 'Mariana';
+let nome2 = 'Nana';
+
+
+console.log(nome);
+console.log(nome2);
+
+console.log(nome, nome2);
+
+//Exemplo de variável const
+//const fruta = ['Graviola', 'Kiwi', 'Pitaia'];
+
+const fruta = 'Graviola';
+const fruta2 = 'Kiwi';
+const fruta3 = 'Pitaia';
+
+//console.log(fruta [2]);
+console.log(fruta);
+console.log(fruta2);
+console.log(fruta3);
