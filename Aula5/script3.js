70d9d310580db0c87c1e7c6008b28eb613f33a96
Mariana Fernandes <marianaflopes155@gmail.com>
2026-03-02 22:03:00 -0300

    While

---
+++ Aula5/script3.js
@@ -0,0 +1,28 @@
+//Introdução Laços de repetição
+
+//Exemplo de For Loop
+
+// let valor = 0;
+// valor = valor + 1;
+
+for (let valor = 0; valor < 10; valor++){
+    //console.log(valor)
+}
+
+//Exemplo de While Loop
+
+let acumuladora = 0;
+
+while (acumuladora < 10) {
+    console.log("O valor da acumuladora agora é de : " + acumuladora);
+    acumuladora = acumuladora +1;
+}
+
+
+//Exemplo de Do While
+let acumuladora2 = 0;
+
+do{
+    console.log("O valor da acumuladora agora é de: "+ acumuladora2);
+    acumuladora2 = acumuladora2 + 1;
+}while(acumuladora2 <= 10);
