230b2a57b067af68a0a79376fa96f5a155e88a44
Mariana Fernandes <marianaflopes155@gmail.com>
2026-03-09 19:56:59 -0300

    Aula 9 de métodos

---
+++ Aula9/script.js
@@ -0,0 +1,30 @@
+//Utilização do métodod toString()
+let numero = 123;
+let exToString = numero.toString;
+console.log(typeof exToString);
+
+// Utilização do toExponential()
+let a = 9.646;
+let extoExponential = a.toExponential(2);
+console.log(extoExponential);
+
+//Utilização do método  toFixed()
+let b = 44.7654987;
+let exToFixed = b.toFixed(2);
+console.log(exToFixed);
+
+//Utilizaçã do método toPrecision()
+let c = 63.874632901;
+let exToPrecision = c.toPrecision(4);
+console.log(exToPrecision);
+
+//Utilizaçã do método valueOf()
+let d = 1993;
+let exValueOf = d.valueOf();
+console.log(exValueOf);
+
+//Utilização do método Number()
+let e = "593";
+let exNumber = numero(e);
+console.log(exNumber);
+
