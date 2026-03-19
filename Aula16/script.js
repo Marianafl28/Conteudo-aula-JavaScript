bccff45379ada0a673f590dc4aa9bca1cb9edb3f
Mariana Fernandes <marianaflopes155@gmail.com>
2026-03-19 19:29:11 -0300

    Aula16 - Finalizando alterações em HTML

---
+++ Aula16/script.js
@@ -0,0 +1,14 @@
+ // Alterando atributo
+
+ const elemento = document.querySelector(".ex1");
+
+ elemento.src = "cabra.jpg";
+ elemento.width = 600;
+
+ //Conteudo dinâmico (data)
+ const elemento2 = document.querySelector("#ex2");
+
+ elemento2.innerHTML = "Data atual: " + Date();
+
+
+
